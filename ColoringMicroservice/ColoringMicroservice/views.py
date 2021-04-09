8ff73ac03d9ce84fa068ceb9b0df8f3991dc2d79
Aleksa <aleksa.novcic@rt-rk.com>
2021-04-09 13:24:11 +0200

    Fix

--- ColoringMicroservice/ColoringMicroservice/views.py
+++ ColoringMicroservice/ColoringMicroservice/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def my_view(request):
-    return HttpResponse('<h1>Page was found MAJKE MI</h1>')
+    return HttpResponse('<h1>Page was found MAJKE MI 123</h1>')
